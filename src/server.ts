b276ba9f6d545d9c7b3d6e5a4b4c5c5f1aadee4f
Bruno Vallim <brunogvallim@gmail.com>
2022-04-07 17:39:49 -0300

    alterando server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import  express, { Router }  from "express";
 import "./database";
 import { routes } from "./routes";
+import { env } from "process";
 
 const app = express();
 
@@ -9,4 +10,4 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3000, () => console.log("server is running port 3000"))
+app.listen(process.env.PORT || 3000, () => console.log("server is running port 3000"))
